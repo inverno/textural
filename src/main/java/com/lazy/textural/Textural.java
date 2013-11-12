2dcb63483f46e7f2da14916cc2e0abdc0282b8f8
inverno <carlo.bottiglieri@gmail.com>
2013-11-12 20:17:24 +0100

    First test, red.

---
+++ src/main/java/com/lazy/textural/Textural.java
@@ -0,0 +1,11 @@
+package com.lazy.textural;
+
+public class Textural {
+    public Textural(String name) {
+
+    }
+
+    public void print(int x0, int y0, int width, int height) {
+        throw new UnsupportedOperationException();
+    }
+}
