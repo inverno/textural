671c773114536d1b2a19c0af77b648b5ce64aaa8
inverno <carlo.bottiglieri@gmail.com>
2013-11-12 20:22:41 +0100

    Still red, I passed the parameters in the wrong order.

--- src/main/java/com/lazy/textural/Textural.java
+++ src/main/java/com/lazy/textural/Textural.java
@@ -1,11 +1,23 @@
 package com.lazy.textural;
 
-public class Textural {
-    public Textural(String name) {
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+public class Textural {
+    private String name;
+
+    public Textural(String name) {
+        this.name = name;
     }
 
     public void print(int x0, int y0, int width, int height) {
-        throw new UnsupportedOperationException();
+        File output = new File(name);
+        try {
+            ImageIO.write(new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB), "bmp", output);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
