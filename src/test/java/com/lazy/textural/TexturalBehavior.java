54fcce443c9e478d29a459378985e5e61eeb88ee
inverno <carlo.bottiglieri@gmail.com>
2013-11-16 18:46:29 +0100

    Removing some duplication.

--- src/test/java/com/lazy/textural/TexturalBehavior.java
+++ src/test/java/com/lazy/textural/TexturalBehavior.java
@@ -4,6 +4,7 @@ import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -11,19 +12,23 @@ import static org.junit.Assert.assertEquals;
 
 public class TexturalBehavior {
 
+    private final String images = "src/test/images/";
+    private final int black = 0xff000000;
+
     @Before
     public void cleanImages() throws IOException {
-        FileUtils.cleanDirectory(new File("src/test/images/"));
+        FileUtils.cleanDirectory(new File(images));
     }
 
     @Test
     public void printsBlackTexture() throws IOException {
-        Textural textural = new Textural("src/test/images/black.png");
+        Textural textural = new Textural(images + "black.png");
         textural.print(100,100);
-        assertEquals(0xff000000, textural.retrieveImage().getRGB(0, 0));
-        assertEquals(0xff000000, textural.retrieveImage().getRGB(99, 0));
-        assertEquals(0xff000000, textural.retrieveImage().getRGB(0, 99));
-        assertEquals(0xff000000, textural.retrieveImage().getRGB(99, 99));
+        final BufferedImage blackTexture = textural.retrieveImage();
+        assertEquals(black, blackTexture.getRGB(0, 0));
+        assertEquals(black, blackTexture.getRGB(99, 0));
+        assertEquals(black, blackTexture.getRGB(0, 99));
+        assertEquals(black, blackTexture.getRGB(99, 99));
     }
 
 }
