65990612b6a6950ba61cee2f1c1303ef47f226af
inverno <carlo.bottiglieri@gmail.com>
2013-11-28 21:14:47 +0100

    Merged

--- src/test/java/com/lazy/textural/TexturalBehavior.java
+++ src/test/java/com/lazy/textural/TexturalBehavior.java
@@ -2,6 +2,7 @@ package com.lazy.textural;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.awt.image.BufferedImage;
@@ -12,12 +13,12 @@ import static org.junit.Assert.assertEquals;
 
 public class TexturalBehavior {
 
-    private final String images = "src/test/images/";
+    private static final String images = "src/test/images/";
     private final int transparent = 0x00000000;
     private final int blue = 0x00ff0000;
 
-    @Before
-    public void cleanImages() throws IOException {
+    @BeforeClass
+    public static void cleanImages() throws IOException {
         FileUtils.cleanDirectory(new File(images));
     }
 
@@ -26,10 +27,7 @@ public class TexturalBehavior {
         Textural textural = new Textural(images + "transparent.png");
         textural.print(100,100);
         final BufferedImage transparentTexture = textural.retrieveImage();
-        assertEquals(transparent, transparentTexture.getRGB(0, 0));
-        assertEquals(transparent, transparentTexture.getRGB(99, 0));
-        assertEquals(transparent, transparentTexture.getRGB(0, 99));
-        assertEquals(transparent, transparentTexture.getRGB(99, 99));
+        assertColorEquals(transparent, transparentTexture);
     }
 
     @Test
@@ -37,10 +35,24 @@ public class TexturalBehavior {
         Textural textural = new Textural(images + "blue.png");
         textural.color(blue).print(100, 100);
         final BufferedImage blueTexture = textural.retrieveImage();
+        assertColorEquals(blue, blueTexture);
+    }
+
+    @Test
+    public void printsHalfHalf() throws Exception {
+        final Textural textural = new Textural(images + "halfhalf.png");
+        textural.brush(new HalfBrush());
+    }
+
+    private void assertColorEquals(int expectedColor, BufferedImage texture) {
         for(int x = 0; x < 100; x++) {
             for(int y = 0; y < 100; y++) {
-                assertEquals("Pixel[" + x + "," + y + "]", blue, blueTexture.getRGB(x, y));
+                assertPixelColorEquals(expectedColor, texture, x, y);
             }
         }
+    }
+
+    private void assertPixelColorEquals(int expectedColor, BufferedImage texture, int x, int y) {
+        assertEquals("Pixel[" + x + "," + y + "]", expectedColor, texture.getRGB(x, y));
     }
 }
